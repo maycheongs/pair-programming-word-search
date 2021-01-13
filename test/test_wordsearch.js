e120b4955dafc10e35bb6fa76f4fb74e129c06ba
May C <maycheongs@gmail.com>
2021-01-13 22:17:23 +0000

    implemented vertical

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,8 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  
+ it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,7 +33,45 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
-
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['V', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ALVIN')
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is empty", function() {
+    const result = wordSearch([
+      []
+    ], 'ALVIN')
+    assert.isFalse(result);
+  })
+
+  it("should return false if the word is empty string ", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['V', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['I', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['N', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+    assert.isFalse(result);
+  })
+
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,54 @@
+const convertVertical = letters => {
+
+    let vertical = [];
+      
+      for (const row in letters[0]) { //0,1
+        let string = '';
+        for (const col in letters) { //0,1,2
+          string +=(letters[col][row])
+        }
+        vertical.push(string)
+      } 
+    
+      return vertical
+    }
+
 const wordSearch = (letters, word) => { 
+    if (!word) return false;
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    }
+    };
+
+    const verticalJoin = convertVertical(letters);
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    };
+    return false
 }
 
 module.exports = wordSearch
+
+// ['A', 'W', 'C'],  ***ROW1
+// ['S', 'E', 'I'],  ***ROW2
+// ['Y', 'F', 'C'],
+// ['N', 'H', 'C']
+
+
+//DIAGONAL FROM TOP RIGHT TO BOTTOM LEFT  
+//EXPCTED RESULT = ['A','WS','CEY','IF','C']
+//...foreach n in matrix[0]
+//'A'  ----   'matrix[0][0]',    n = 0
+//'WS' ---- matrix[0][1] + matrix[1][0]' 'WS' n = 1
+//'CEY' ---- matrix[0][2] + matrix[1][1] + matrix [2][0]'  n = 2 ... 
+
+// matrix[1][2] + matrix[2][1] + matrix[3][0]     'IFN'
+// matrix[2][2] + matrix[3][1]                  'CH'
+// matrix[3][2]                                 'C'
+
+
+//loop first array
+//loop second array
+//if the index of the second array is same as first array
+//compare the string to word
+//if not then add matrix[i - i][j + 1]
